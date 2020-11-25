91b5c148692a8d0906db84fe0cdab38fe434f8a4
jaripeks <jari.p.salo@gmail.com>
2020-11-25 14:53:06 +0200

    exercise 7.3 done

---
+++ osa7/routed-anecdotes/src/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import {
+  Switch, Route, useRouteMatch
+} from 'react-router-dom'
+import Menu from './components/Menu'
+import About from './components/About'
+import Footer from './components/Footer'
+import AnecdoteList from './components/AnecdoteList'
+import Anecdote from './components/Anecdote'
+import CreateNew from './components/CreateNew'
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: '1'
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: '2'
+    }
+  ])
+
+  const [notification, setNotification] = useState('')
+
+  const anecdoteById = (id) => anecdotes.find(a => a.id === id)
+
+  const match = useRouteMatch('/anecdotes/:id')
+  const anecdote = match
+    ? anecdoteById(match.params.id)
+    : null
+
+  const addNew = (anecdote) => {
+    anecdote.id = (Math.random() * 10000).toFixed(0)
+    setAnecdotes(anecdotes.concat(anecdote))
+    setNotification(`a new anecdote ${anecdote.content} created!`)
+    setTimeout(() => {
+      setNotification('')
+    }, 10000)
+  }
+
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+        <Menu />
+        {notification}
+        <Switch>
+          <Route path="/anecdotes/:id">
+            <Anecdote anecdote={anecdote} />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/create">
+            <CreateNew addNew={addNew} />
+          </Route>
+          <Route path="/">
+            <AnecdoteList anecdotes={anecdotes} />
+          </Route>
+        </Switch>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ osa7/routed-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const AnecdoteList = ({ anecdotes }) => {
+    return (
+        <div>
+            <div>
+                <h2>Anecdotes</h2>
+                <ul>
+                    {anecdotes.map(anecdote =>
+                        <li key={anecdote.id} >
+                            <Link to={`/anecdotes/${anecdote.id}`}>
+                                {anecdote.content}
+                            </Link>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default AnecdoteList
